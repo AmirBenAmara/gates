2ae826aab5d4f81761ddaad33a6a397eb8e2426c
AmirBenAmara <amir.benamara@esprit.tn>
2023-02-20 12:27:15 +0000

    user add

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cilPencil, cilTrash, cilPlus } from '@coreui/icons';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+  icons = { cilPencil, cilTrash ,cilPlus};
+  public visible = false;
 
+  openUserModal() {
+    this.visible = true;
+  }
+  cancel() {
+    this.visible = false;
+  }
+
+  handleUserModalVisbilityChange(event: any) {
+    this.visible = event;
+  }
 }
