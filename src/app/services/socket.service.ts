03d5042012a6ec0b205bea86f84b5ddfad1e5967
AmirBenAmara <amir.benamara@esprit.tn>
2023-04-28 16:50:43 +0100

    socket

---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  socket: any;
+  readonly url: string = "http://localhost:8080";
+
+  constructor() { 
+    this.socket = io(this.url);
+  }
+
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data: any) => {
+        subscriber.next(data);
+      });
+    });
+  }
+
+  emit(eventName: string, data: any) {
+    this.socket.emit(eventName, data);
+  }
+}
