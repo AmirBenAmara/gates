add30f4ab04cdbd1ad80ceff98d1245ece5393e5
AmirBenAmara <amir.benamara@esprit.tn>
2023-07-18 22:45:12 +0100

    users

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class UsersService {
-  apiUrl: string = environment.apiUrl + 'User';
+  apiUrl: string = environment.apiUrl + 'authenticate/';
 
   constructor(private http: HttpClient) { }
 
@@ -17,5 +17,43 @@ export class UsersService {
   login(loginDTO: any) {
     return this.http.post<void>(this.apiUrl + '/login', loginDTO);
   }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}`);
+    // return of(DepartmentsDATA);
+  }
 
+  getUserById(userId: number): Observable<User[]> {
+    const url = `${this.apiUrl}/${userId}`;
+    return this.http.get<User[]>(url);
+  }
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl + 'addUser', user);
+  }
+  createAdmin(admin: Admin): Observable<Admin> {
+    return this.http.post<User>(this.apiUrl+ 'addAdmin', admin);
+  }
+
+  updateUser(
+    userId: number,
+    user: User
+  ): Observable<void> {
+    const url = `${this.apiUrl}/${userId}`;
+    return this.http.put<void>(url, user);
+  }
+
+  deleteUser(userId: number): Observable<void> {
+    const url = `${this.apiUrl}/${userId}`;
+    return this.http.delete<void>(url);
+  }
+}
+
+export interface User {
+  name: string;
+  email: String;
+  password: String;
+}
+export interface Admin {
+  name: string;
+  email: String;
+  password: String;
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,24 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { cilPencil, cilTrash, cilPlus, cilInfo } from '@coreui/icons';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
-  icons = { cilPencil, cilTrash ,cilPlus ,cilInfo};
+export class UsersComponent implements OnInit {
+  userForm: FormGroup;
+  adminForm: FormGroup;
+  private fb: FormBuilder;
+  icons = { cilPencil, cilTrash, cilPlus, cilInfo };
   public visible = false;
+  public visibleAdmin = false;
   public viewModalDeleteVisible = false;
+  
+  constructor(
+    private userservice: UsersService,
+  ) {}
+
+  ngOnInit(): void {
+    this.userForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+    this.adminForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
+  }
+ 
 
   openUserModal() {
     this.visible = true;
   }
+  openAdminModal() {
+    this.visibleAdmin = true;
+  }
   cancel() {
     this.visible = false;
+    this.visibleAdmin = false;
   }
 
   handleUserModalVisbilityChange(event: any) {
     this.visible = event;
+  }
+  handleAdminModalVisbilityChange(event: any) {
+    this.visibleAdmin = event;
+  }
+
+  submitUser(){
+    this.userservice.createUser(this.userForm.value).subscribe(res => {
+      this.cancel()
+    })
+  }
+  submitAdmin(){
+    this.userservice.createAdmin(this.adminForm.value).subscribe(res => {
+      this.cancel()
+    })
   }
 }
